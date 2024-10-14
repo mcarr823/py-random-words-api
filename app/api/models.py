b46bfd42145f20a865fa72ee93bffdff0b09189e
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-14 18:23:54 +1100

    Added model and initial data migration

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Word(models.Model):
+    text = models.CharField(max_length=30)
+    length = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
