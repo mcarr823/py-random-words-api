1a8bc0169e4013a093c3b640f95a3e3471c008f0
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-14 18:24:18 +1100

    Added main endpoints

--- app/api/urls.py
+++ app/api/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("word/", views.random, name="random"),
+    path("word/<int:length>/", views.randomLength, name="randomLength"),
+    path("words/", views.all, name="all"),
+    path("words/<int:length>/", views.allLength, name="allLength"),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,37 @@
 from django.http import HttpResponse, JsonResponse
+from random import choice
+from .models import Word
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def random(request):
+    # Grab a list of primary keys, instead of querying the objects,
+    # for efficiency reasons
+    pks = Word.objects.values_list('pk', flat=True)
+
+    # Pick a random primary key
+    random_pk = choice(pks)
+
+    # Grab the object which corresponds to this primary key
+    word = Word.objects.get(pk=random_pk)
+
+    # And return it
+    return JsonResponse([word.text], safe=False)
+
+def randomLength(request, length):
+    pks = Word.objects.filter(length=length).values_list('pk', flat=True)
+    random_pk = choice(pks)
+    word = Word.objects.get(pk=random_pk)
+    return JsonResponse([word.text], safe=False)
+
+def all(request):
+    words = Word.objects.values_list('text', flat=True)
+    words = list(words)
+    return JsonResponse(words, safe=False)
+
+def allLength(request, length):
+    words = Word.objects.filter(length=length).values_list('text', flat=True)
+    words = list(words)
+    return JsonResponse(words, safe=False)
